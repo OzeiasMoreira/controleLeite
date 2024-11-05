faaac0358094c90930bfb36bbe046d5f86b72800
OzeiasMoreira <ozmoreira17@gmail.com>
2024-11-05 11:14:52 -0300

    first commit

---
+++ demo/src/main/java/controle/leite/SecondaryController.java
@@ -0,0 +1,12 @@
+package controle.leite;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ demo/src/test/java/controle/leite/banco/DaoTest.java
@@ -0,0 +1,19 @@
+package controle.leite.banco;
+
+import org.junit.jupiter.api.Test;
+
+import controle.leite.modelo.Usuario;
+import controle.leite.modelo.Vaca;
+
+public class DaoTest {
+    @Test
+    void testInserir() {
+        Usuario u = new Usuario("Ozeias","123","123");
+        Vaca v = new Vaca("1","Generica", "Nelore");
+        Dao<Usuario> daou = new Dao(Usuario.class);
+        Dao<Vaca> daov = new Dao(Vaca.class);
+        daou.inserir(u);
+        daov.inserir(v);
+
+    }
+}
