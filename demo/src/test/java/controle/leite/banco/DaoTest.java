f72e300974065f073bfd4af7ca0c77905f0c4d82
OzeiasMoreira <ozmoreira17@gmail.com>
2024-11-05 11:36:56 -0300

    adicionando tela de login

--- demo/src/test/java/controle/leite/banco/DaoTest.java
+++ demo/src/test/java/controle/leite/banco/DaoTest.java
@@ -6,14 +6,14 @@ import controle.leite.modelo.Usuario;
 import controle.leite.modelo.Vaca;
 
 public class DaoTest {
-    @Test
-    void testInserir() {
-        Usuario u = new Usuario("Ozeias","123","123");
-        Vaca v = new Vaca("1","Generica", "Nelore");
-        Dao<Usuario> daou = new Dao(Usuario.class);
-        Dao<Vaca> daov = new Dao(Vaca.class);
-        daou.inserir(u);
-        daov.inserir(v);
+    // @Test
+    // void testInserir() {
+    //     Usuario u = new Usuario("Ozeias","123","123");
+    //     Vaca v = new Vaca("1","Generica", "Nelore");
+    //     Dao<Usuario> daou = new Dao(Usuario.class);
+    //     Dao<Vaca> daov = new Dao(Vaca.class);
+    //     daou.inserir(u);
+    //     daov.inserir(v);
 
-    }
+    // }
 }
